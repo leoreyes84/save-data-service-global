4f0512cb2d6fe68bdb301b1993587845eae547e4
Leonardo Reyes <leonardo.reyes@colfuturo.org>
2020-01-26 17:08:43 -0500

    Iniciación proyecto

---
+++ src/main/java/com/global/prueba/savedataservice/SaveDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.global.prueba.savedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/global/prueba/savedataservice/SaveDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.prueba.savedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
